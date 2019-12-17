d8ef554c4e0e7e62d1645ab681d4a77338a6e38c
Ghost <ghosttw88@hotmail.com>
2019-12-18 00:47:00 +0800

    nunt-testfixture

---
+++ nunt-testfixture/NUnitTestProject/NunitUnitTest.cs
@@ -0,0 +1,85 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace TestFrameworksProject
+{
+    [TestFixture]
+    public class NunitUnitTest
+    {
+        public List<int> list;
+
+        public List<int> listSetup;
+
+        public List<int> listOneTimeSetup;
+
+        public NunitUnitTest()
+        {
+            list = new List<int>();
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeInit() => listOneTimeSetup = new List<int>();
+
+        [SetUp]
+        public void Init() => listSetup = new List<int>();
+
+        [Parallelizable]
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(9)]
+        [TestCase(10)]
+        [TestCase(11)]
+        [TestCase(12)]
+        [TestCase(13)]
+        [TestCase(14)]
+        [TestCase(15)]
+        [TestCase(16)]
+        public void TestA(int i)
+        {
+            list.Add(i);
+            listSetup.Add(i);
+            listOneTimeSetup.Add(i);
+            Console.WriteLine(list.Count);
+            Console.WriteLine(listOneTimeSetup.Count);
+            Assert.AreEqual(1, listSetup.Count);
+            Assert.AreEqual(list.Count, listOneTimeSetup.Count);
+        }
+
+        [Parallelizable(ParallelScope.All)]
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(9)]
+        [TestCase(10)]
+        [TestCase(11)]
+        [TestCase(12)]
+        [TestCase(13)]
+        [TestCase(14)]
+        [TestCase(15)]
+        [TestCase(16)]
+        public void TestB(int i)
+        {
+            list.Add(i);
+            listSetup.Add(i);
+            listOneTimeSetup.Add(i);
+            Console.WriteLine(list.Count);
+            Console.WriteLine(listOneTimeSetup.Count);
+            Assert.AreEqual(1, listSetup.Count);
+            Assert.AreEqual(list.Count, listOneTimeSetup.Count);
+        }
+    }
+}
---
+++ nunt-testfixture/NUnitTestProject/xUnitUnitTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TestFrameworksProject
+{
+    public class xUnitUnitTest
+    {
+        public List<int> list = new List<int>();
+
+        public List<int> listSetup = new List<int>();
+
+        public List<int> listOneTimeSetup = new List<int>();
+        private ITestOutputHelper output;
+
+        public xUnitUnitTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        [InlineData(11)]
+        [InlineData(12)]
+        [InlineData(13)]
+        [InlineData(14)]
+        [InlineData(15)]
+        [InlineData(16)]
+        public void TestA(int i)
+        {
+            list.Add(i);
+            listSetup.Add(i);
+            listOneTimeSetup.Add(i);
+            output.WriteLine(list.Count.ToString());
+            output.WriteLine(listOneTimeSetup.Count.ToString());
+            Assert.Single(listSetup);
+            Assert.Equal(list.Count, listOneTimeSetup.Count);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        [InlineData(11)]
+        [InlineData(12)]
+        [InlineData(13)]
+        [InlineData(14)]
+        [InlineData(15)]
+        [InlineData(16)]
+        public void TestB(int i)
+        {
+            list.Add(i);
+            listSetup.Add(i);
+            listOneTimeSetup.Add(i);
+            output.WriteLine(list.Count.ToString());
+            output.WriteLine(listOneTimeSetup.Count.ToString());
+            Assert.Single(listSetup);
+            Assert.Equal(list.Count, listOneTimeSetup.Count);
+        }
+    }
+}
